ba5eeb3c89f16dd3b2ce7e15327b807d9c7220c3
goldenCitrus <goldenpotatobull@outlook.com>
2026-01-13 22:06:18 -0700

    finished week 2 ponder, started working on a home menu dropdown nav system

--- week-2/ponder/week2.js
+++ week-2/ponder/week2.js
@@ -1,13 +1,25 @@
 let selectElem = document.getElementById('webdevlist');
-let ogValue = {};
+
+const ogText = {
+    'html': document.getElementById('html').textContent,
+    'css': document.getElementById('css').textContent,
+    'js': document.getElementById('js').textContent
+};
+
+let changedItemID = '';
 
 function functionMode() {
-    console.log(ogValue)
+    if (changedItemID !== '') {
+        document.getElementById(changedItemID).textContent = ogText[changedItemID];
+        document.getElementById(changedItemID).style.color = 'black';
+    }
     let codeValue = selectElem.value;
     console.log(codeValue);
     document.getElementById(codeValue).textContent = "EVIL " + document.getElementById(codeValue).textContent;
-    ogValue = document.getElementById(codeValue).textContent
-    console.log(ogValue)
+    document.getElementById(codeValue).style.color = 'red';
+    // document.getElementById('thunder').play();
+    document.getElementsByClassName(codeValue)[0].play();
+    changedItemID = codeValue;
 }
 selectElem.addEventListener('change', functionMode);
                 

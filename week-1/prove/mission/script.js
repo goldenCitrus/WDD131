76c4b9f9c9d163a0ee83173955e5a0f7a4eabb6a
goldenCitrus <goldenpotatobull@outlook.com>
2026-01-17 19:50:18 -0700

    Iterated on mission statement to complete prove week 2. still listed as prove week 1 under landing page directory. gotta fix that

---
+++ week-1/prove/mission/script.js
@@ -0,0 +1,32 @@
+
+let selectElem = document.getElementById('menu');
+let logo = document.querySelector('img');
+let texts = document.querySelectorAll('h1, p, li');
+// let ps = document.querySelectorAll('p');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+        console.log(current);
+
+    if (current === 'dark') {
+        document.body.style.backgroundColor = "#333333";
+        document.body.style.borderColor = 'white';
+        for (let i = 0; i < texts.length; i++) {
+            if (texts[i].textContent !== 'BRIGHAM YOUNG UNIVERSITY-IDAHO') {
+                texts[i].style.color = 'white';
+            }
+        logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-white.png'
+        }
+    } else if (current === 'light') {
+        document.body.style.backgroundColor = "white";
+        document.body.style.borderColor = '#c2c2c2';
+        for (let i = 0; i < texts.length; i++) {
+            if (texts[i].textContent !== 'BRIGHAM YOUNG UNIVERSITY-IDAHO') {
+                texts[i].style.color = 'black';
+            }
+        logo.src = 'https://wddbyui.github.io/wdd131/images/byui-logo-blue.webp'
+        }
+    }
+}

1f64dedac673986803b8c8052cba3f85c51cac42
goldenCitrus <goldenpotatobull@outlook.com>
2026-01-20 15:50:12 -0700

    finished W03 Ponder

---
+++ week-3/ponder/script.js
@@ -0,0 +1,19 @@
+let hamMenu = document.querySelector('.menu-btn');
+const navLinks = document.querySelectorAll('nav a');
+
+hamMenu.addEventListener('click', navMenu);  
+
+function navMenu() {
+    console.log('got it')
+    // document.getElementById('boing').stop();
+    document.getElementById('boing').play();
+    for (let i = 0; i < navLinks.length; i++) {
+        if (navLinks[i].style.display === 'flex') {
+            navLinks[i].style.display = 'none';
+        } else {
+            navLinks[i].style.display = 'flex';
+            navLinks[i].style.flexDirection = 'row';
+        }
+    }
+}
+

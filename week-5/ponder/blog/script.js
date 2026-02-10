1f7b52555a59751297df53895cbdc8f177d5fd18
golden_ <goldenpotatobull@outlook.com>
2026-02-10 09:47:23 -0700

    updated home page, and changed a variable to const in week05 ponder js

--- week-5/ponder/blog/script.js
+++ week-5/ponder/blog/script.js
@@ -1,6 +1,6 @@
 const rating = document.querySelectorAll('#starRating');
 
 rating.forEach(star => {
-    let stars = star.textContent.length;
+    const stars = star.textContent.length;
     star.setAttribute('aria-label', `Rating: ${stars} out of 5 stars`);
 });

d2d7a1eac43258b1791f89fa16795c9849972ce0
golden_ <goldenpotatobull@outlook.com>
2026-02-10 10:02:32 -0700

    added click animation to home hamMenu

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 let hamMenu = document.querySelector('.menu-btn');
 const navigate = document.getElementById('navMenu');
+const navLinks = document.querySelectorAll('nav a');
+
 
 hamMenu.addEventListener('click', () => { navigate.classList.toggle('open'); }); 
+
+
+hamMenu.addEventListener('click', navMenu);  
+
+function navMenu() {
+    // console.log('got it')
+    hamMenu.classList.toggle('click')
+    // document.getElementById('boing').stop();
+    // document.getElementById('boing').play();
+    // for (let i = 0; i < navLinks.length; i++) {
+    //     if (navLinks[i].style.display === 'flex') {
+    //         navLinks[i].style.display = 'none';
+    //     } else {
+    //         navLinks[i].style.display = 'flex';
+    //         navLinks[i].style.flexDirection = 'row';
+    //     }
+    // }
+}
 
 // function navMenu() {
 //     console.log('got it')
